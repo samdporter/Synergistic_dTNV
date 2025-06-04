4c49a74c45f67b15a6681c1f9b804b3548eb50b2
Sam Porter <sam.porter.18@ucl.ac.uk>
2025-06-04 15:55:18 +0100

    updated useful helper functions

--- main_functions.py
+++ main_functions.py
@@ -1,5 +1,6 @@
 # import method type
 from types import MethodType
+from cil.optimisation.functions import OperatorCompositionFunction, ScaledFunction
 
 def compute_kappa_squared_image_from_partitioned_objective(obj_funs, initial_image, normalise=True):
     """
@@ -12,7 +13,13 @@ def compute_kappa_squared_image_from_partitioned_objective(obj_funs, initial_ima
     out = initial_image.get_uniform_copy(0)
     for obj_fun in obj_funs:
         # need to get the function from the ScaledFunction OperatorCompositionFunction
-        out += obj_fun.function.multiply_with_Hessian(initial_image, initial_image.allocate(1))
+        if isinstance(obj_fun.function, OperatorCompositionFunction):
+            out += obj_fun.function.function.multiply_with_Hessian(
+                initial_image,
+                initial_image.allocate(1)
+            )
+        else:
+            out += obj_fun.function.multiply_with_Hessian(initial_image, initial_image.allocate(1))
     out = out.abs()
     # shouldn't really need to do this, but just in case
     out = out.maximum(0)
@@ -21,7 +28,12 @@ def compute_kappa_squared_image_from_partitioned_objective(obj_funs, initial_ima
     mean = out.sum()/out.size
     print(f"mean: {mean}")
     if normalise:
-        out /= mean
+        # we want to normalise by thye median
+        median = out.as_array().flatten()
+        median.sort()
+        median = median[int(median.size/2)]
+        print(f"median: {median}")
+        out /= median
     return out
 
 def attach_prior_hessian(prior, epsilon = 0) -> None:
@@ -33,8 +45,21 @@ def attach_prior_hessian(prior, epsilon = 0) -> None:
                 self.function.operator.direct(x),
             )
         )
+        ret = ret.abs()
+        if out is not None:
+            out.fill(ret)
+        return ret
+    
+    def hessian_diag(self, x, out=None, epsilon=epsilon):
+        ret = self.function.operator.adjoint(
+            self.function.function.hessian_diag(
+                self.function.operator.direct(x),
+            )
+        )
+        ret = ret.abs()
         if out is not None:
             out.fill(ret)
         return ret
 
     prior.inv_hessian_diag = MethodType(inv_hessian_diag, prior)
+    prior.hessian_diag = MethodType(hessian_diag, prior)
